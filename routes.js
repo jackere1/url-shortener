b45551b260d3f156d581ee77ca5f2d15d1c19385
enkhbold.n <enkhbold.n@corp.unitel.mn>
2024-08-14 11:49:13 +0800

    Init without image

--- routes.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = function (app, opts) {
-  // Setup routes, middleware, and handlers
-  app.get('/', (req, res) => {
-    res.locals.name = 'url-shortener'
-    res.render('index')
-  })
-}
