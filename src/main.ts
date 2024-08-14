22092c75beb123a5f22b1e54ec8bba546948cb86
enkhbold.n <enkhbold.n@corp.unitel.mn>
2024-08-14 13:11:52 +0800

    added port env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "module-alias/register";
+import "dotenv/config";
 import app from "@app";
 import { connectToDB } from "@config";
 
-app.listen(3000, async () => {
+app.listen(process.env.PORT || 3000, async () => {
   await connectToDB();
-  console.log("Server is running on 3000");
+  console.log("Server is running on " + process.env.PORT || 3000);
 });
